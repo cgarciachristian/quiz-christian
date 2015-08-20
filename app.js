3f497f7fdf977ae2b9cc9024e00ea6996b7fa433
Christian Garcia <cgarciachristian@gmail.com>
2015-08-20 03:20:09 -0500

    Proyecto final con autologout a los 2 minutos

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 
@@ -21,9 +22,37 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    var tiempoActual = new Date().getTime();
+    var tiempoUltimaTrans;
+    var transcurrido;
+    if(req.session.user){
+        if(!req.session.ultimaTrans)
+            req.session.ultimaTrans = tiempoActual;
+        tiempoUltimaTrans = req.session.ultimaTrans;
+        transcurrido = (tiempoActual - tiempoUltimaTrans) / 1000;
+        if(transcurrido > 120){
+            req.session.ultimaTrans = null;
+            delete req.session.user;
+        }
+        console.log("tiempo transcurrido " + transcurrido);
+    }
+    next();   
+});
+
+app.use(function(req, res, next){
+    if(!req.path.match(/\/login|\/logout/)){
+        req.session.redir = req.path;
+    }
+    res.locals.session = req.session;
+    next();
+});
 
 app.use('/', routes);
 
